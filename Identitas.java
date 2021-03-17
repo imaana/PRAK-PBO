9c95cc278ac18fefb8497bf96d621bb828eadd3a
Afifah Ghaisani Imana <47937079+imaana@users.noreply.github.com>
2021-03-17 08:01:29 +0700

    Add files via upload

---
+++ Identitas.java
@@ -0,0 +1,8 @@
+public class Identitas{
+	public static void main(String[]args){
+		System.out.println("Ganti dengan NAMA Anda");
+		System.out.println("Ganti dengan NIM Anda");
+		System.out.println("Saya akan lulus kuliah daring ini");
+	}
+}
+
---
+++ Nama_File.java
@@ -0,0 +1,7 @@
+public class Nama_File
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Program Pertama Java");
+	}
+}
